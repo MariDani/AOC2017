ee2b34fc8e762e96f3ee37372b2356dfa06e5dd6
Mariana Danielova <mariana.danielova@eli-beams.eu>
2017-12-08 20:35:16 +0100

    Day 7 part one solution update

--- script7.js
+++ script7.js
@@ -1,136 +1,48 @@
-function solvePrograms(data) {
-    
-    // loop through each row of the data (program)
-    let programs = [];
-    programs = createAllPrograms(programs, data)
+const regex = /([a-z]+)\s\D(\d+)\D(\s->\s(\S.+))?/s;
+
+let structures = []
+structures = loadStructures(input, regex, structures)
+
+let nodeList = []
+nodeList = createNodeList(structures, nodeList)
+
+const root = findRoot(structures, nodeList)
+console.log("Part One: ", root)
 
 
-    // find only those programs which have subtowers
-    let subtowersPrograms = [];
-    subtowersPrograms = findsubtowersPrograms(programs, subtowersPrograms);
-
-    // find bottom program
-    const bottomProgram = findBottomProgram(subtowersPrograms)
-    // First Part solution
-    console.log("Bottom program is: ", bottomProgram);
-
-    checkWeightBalance(bottomProgram, programs);
-
+function loadStructures(input, regex, structures){
+    let inputArray = [];
+    inputArray = input.split('\n')
+    for (let idx = 0; idx < inputArray.length; idx++){
+        structures.push(regex.exec(inputArray[idx]))
+    }
+    return structures;
 }
 
-function parseData(url, callBack) {
-    Papa.parse(url, {
-        download: true,
-        dynamicTyping: true,
-        delimiter: " ",
-        complete: function(results) {
-            callBack(results.data);
-        }
-    });
-}
 
-parseData("day7input.csv", solvePrograms);
-
-
-
-function createAllPrograms(programs, data){
-    for (let programIdx = 0; programIdx < data.length; programIdx++){
-        programs[programIdx] = {};
-        const program = data[programIdx];
-
-        // extract program's name
-        programs[programIdx].name = program[0];
-
-        // extract program's weight
-        const weight = program[1];
-        programs[programIdx].weight = parseInt(weight.substr(1,2));
-
-        // determine if program has sub-towers
-        if (program[2]){
-            programs[programIdx].subtowers = []
-            for (let idx = 3; idx < program.length; idx ++){
-                const subtowerName = program[idx];
-                programs[programIdx].subtowers[idx-3] = subtowerName.replace(",","");
+function createNodeList(structures, nodeList){
+    for (let idx = 0; idx < structures.length; idx++){
+        const structure = structures[idx]
+        if (structure[4] != undefined){
+            const childRegex = /([a-z]+)+?/g;
+            let nodes = structure[4].match(childRegex);
+            for (let idx = 0; idx < nodes.length; idx++){
+                nodeList.push(nodes[idx])
             }
         }
     }
-    return programs;
+    return nodeList
 }
 
 
-function findsubtowersPrograms(programs, subtowersPrograms){
-    for (let idx = 0; idx < programs.length; idx++){
-        if (programs[idx].subtowers){
-            subtowersPrograms.push(programs[idx])
+function findRoot(structures, nodeList){
+    for (let idx = 0; idx < structures.length; idx++){
+        const structure = structures[idx]
+        if (structure[4] != undefined){
+            if (!nodeList.includes(structure[1])){
+                return structure[1]
+            }
+            
         }
     }
-    return subtowersPrograms;
-}
-
-
-function findBottomProgram(subtowersPrograms){
-    // create array with names of subtowers
-    let subtowers = [];
-    for (let idx = 0; idx < subtowersPrograms.length; idx++){
-        for (let subtowerIdx = 0; subtowerIdx < subtowersPrograms[idx].subtowers.length; subtowerIdx++){
-            subtowers.push(subtowersPrograms[idx].subtowers[subtowerIdx])
-        }
-    }
-
-    for (let idx = 0; idx < subtowersPrograms.length; idx++){
-        const name = subtowersPrograms[idx].name;
-        if (!subtowers.includes(name)){
-            return name
-        }
-    }    
-}
-
-
-function checkWeightBalance(program, programs){
-    let programsStructure = getProgramObject(program, programs);
-    programsStructure = createProgramsStructure(programsStructure, programs);
-
-    console.log(programsStructure);
-
-    let weightSum = [];
-
-    for (let idx = 0; idx < programsStructure.subtowers.length; idx ++){
-        weightSum[idx] = programsStructure.subtowers[idx].weight
-        weightSum[idx] = getProgramsWeight(weightSum[idx], programsStructure.subtowers[idx], programs)
-    }
-
-    console.log(weightSum)
-}
-
-
-function getProgramObject(program, programs){
-    for (let idx = 0; idx < programs.length; idx++){
-        if (program === programs[idx].name){
-            const currentProgram = programs[idx]
-            programs.splice(idx, 1);
-            return currentProgram;
-        }
-    }
-}
-
-
-function createProgramsStructure(parentProgram, programs){
-    if (parentProgram.subtowers){
-        for (let idx = 0; idx < parentProgram.subtowers.length; idx++){
-            parentProgram.subtowers[idx] = getProgramObject(parentProgram.subtowers[idx], programs)
-            parentProgram.subtowers[idx] = createProgramsStructure(parentProgram.subtowers[idx], programs)
-        } 
-    }   
-    return parentProgram
-}
-
-
-function getProgramsWeight(weightSum, parentProgram, programs){
-    if (parentProgram.subtowers){
-        for (let idx = 0; idx < parentProgram.subtowers.length; idx ++){
-            weightSum = weightSum + parentProgram.subtowers[idx].weight
-            weightSum = getProgramsWeight(weightSum, parentProgram.subtowers[idx], programs)
-        }
-    }
-    return weightSum;
 }
